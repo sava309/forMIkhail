e33ecc9858b9c5339a467668e94573a04d37d281
sava309 <61836868+sava309@users.noreply.github.com>
2023-07-13 19:42:06 +0300

    Update main2.go

--- main2.go
+++ main2.go
@@ -1,90 +1,147 @@
 package main
 
 import (
-    "log"
+ "database/sql"
+ "fmt"
+ "log"
 
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/sqlite"
-    "github.com/kylelemons/godebug/pretty"
+ _ "github.com/mattn/go-sqlite3"
 )
 
 // Заказ
 type Order struct {
-    gorm.Model
-    Status     string
-    OrderItems []OrderItem
+ ID         int
+ Status     string
+ OrderItems []OrderItem
 }
 
 // Позиция заказа
 type OrderItem struct {
-    gorm.Model
-    OrderID  uint
-    ItemID   uint
-    Item     Item
-    Quantity int
+ ID        int
+ OrderID   int
+ ItemID    int
+ Quantity  int
+ Item      Item
 }
 
 // Продукты
 type Item struct {
-    gorm.Model
-    ItemName string
-    Amount   float32
+ ID       int
+ ItemName string
+ Amount   float32
 }
 
 var (
-    items = []Item{
-        {ItemName: "Go Mug", Amount: 12.49},
-        {ItemName: "Go Keychain", Amount: 6.95},
-        {ItemName: "Go Tshirt", Amount: 17.99},
-    }
+ items = []Item{
+  {ItemName: "Go Mug", Amount: 12.49},
+  {ItemName: "Go Keychain", Amount: 6.95},
+  {ItemName: "Go Tshirt", Amount: 17.99},
+ }
 )
 
 func main() {
-    db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
-    db.LogMode(true)
-    if err != nil {
-        log.Panic(err)
-    }
-    defer db.Close()
+ db, err := sql.Open("sqlite3", "mydatabase.db")
+ if err != nil {
+  log.Fatal(err)
+ }
+ defer db.Close()
 
-    // Перенос схемы
-    db.AutoMigrate(&OrderItem{}, &Order{}, &Item{})
+ // Создание таблицы "Заказ"
+ _, err = db.Exec(`
+  CREATE TABLE IF NOT EXISTS Заказ (
+   ID INTEGER PRIMARY KEY,
+   Status TEXT
+  )
+ `)
+ if err != nil {
+  log.Fatal(err)
+ }
 
-    // Создание предметов
-    for index := range items {
-        db.Create(&items[index])
-    }
-    order := Order{Status: "pending"}
-    db.Create(&order)
-    item1 := OrderItem{OrderID: order.ID, ItemID: items[0].ID, Quantity: 1}
-    item2 := OrderItem{OrderID: order.ID, ItemID: items[1].ID, Quantity: 4}
-    db.Create(&item1)
-    db.Create(&item2)
+ // Создание таблицы "Позиция заказа"
+ _, err = db.Exec(`
+  CREATE TABLE IF NOT EXISTS Позиция_заказа (
+   ID INTEGER PRIMARY KEY,
+   OrderID INTEGER,
+   ItemID INTEGER,
+   Quantity INTEGER,
+   FOREIGN KEY (OrderID) REFERENCES Заказ(ID)
+  )
+ `)
+ if err != nil {
+  log.Fatal(err)
+ }
 
-    // Запрос с соединениями
-    rows, err := db.Table("orders").Where("orders.id = ? and status = ?", order.ID, "pending").
-        Joins("Join order_items on order_items.order_id = orders.id").
-        Joins("Join items on items.id = order_items.id").
-        Select("orders.id, orders.status, order_items.order_id, order_items.item_id, order_items.quantity" +
-            ", items.item_name, items.amount").Rows()
-    if err != nil {
-        log.Panic(err)
-    }
+ // Создание таблицы "Продукты"
+ _, err = db.Exec(`
+  CREATE TABLE IF NOT EXISTS Продукты (
+   ID INTEGER PRIMARY KEY,
+   ItemName TEXT,
+   Amount REAL
+  )
+ `)
+ if err != nil {
+  log.Fatal(err)
+ }
 
-    defer rows.Close()
-    // Значения для загрузки
-    newOrder := &Order{}
-    newOrder.OrderItems = make([]OrderItem, 0)
+ // Вставка данных в таблицу "Продукты"
+ for _, item := range items {
+  _, err = db.Exec("INSERT INTO Продукты (ItemName, Amount) VALUES (?, ?)",
+   item.ItemName, item.Amount)
+  if err != nil {
+   log.Fatal(err)
+  }
+ }
 
-    for rows.Next() {
-        orderItem := OrderItem{}
-        item := Item{}
-        err = rows.Scan(&newOrder.ID, &newOrder.Status, &orderItem.OrderID, &orderItem.ItemID, &orderItem.Quantity, &item.ItemName, &item.Amount)
-        if err != nil {
-            log.Panic(err)
-        }
-        orderItem.Item = item
-        newOrder.OrderItems = append(newOrder.OrderItems, orderItem)
-    }
-    log.Print(pretty.Sprint(newOrder))
+ // Вставка данных в таблицу "Заказ"
+ order := Order{Status: "pending"}
+ _, err = db.Exec("INSERT INTO Заказ (Status) VALUES (?)", order.Status)
+ if err != nil {
+  log.Fatal(err)
+ }
+
+ // Вставка данных в таблицу "Позиция заказа"
+ item1 := OrderItem{OrderID: 1, ItemID: 1, Quantity: 1}
+ item2 := OrderItem{OrderID: 1, ItemID: 2, Quantity: 4}
+ _, err = db.Exec("INSERT INTO Позиция_заказа (OrderID, ItemID, Quantity) VALUES (?, ?, ?)",
+  item1.OrderID, item1.ItemID, item1.Quantity)
+ if err != nil {
+  log.Fatal(err)
+ }
+ _, err = db.Exec("INSERT INTO Позиция_заказа (OrderID, ItemID, Quantity) VALUES (?, ?, ?)",
+  item2.OrderID, item2.ItemID, item2.Quantity)
+ if err != nil {
+  log.Fatal(err)
+ }
+
+ // Запрос данных о заказе с использованием JOIN
+ rows, err := db.Query(`
+  SELECT Заказ.ID, Заказ.Status, Позиция_заказа.ID, Позиция_заказа.OrderID, Позиция_заказа.ItemID,
+   Позиция_заказа.Quantity, Продукты.ItemName, Продукты.Amount
+  FROM Заказ
+  JOIN Позиция_заказа ON Позиция_заказа.OrderID = Заказ.ID
+  JOIN Продукты ON Продукты.ID = Позиция_заказа.ItemID
+  WHERE Заказ.ID = ? AND Заказ.Status = ?
+ `, order.ID, order.Status)
+ if err != nil {
+  log.Fatal(err)
+ }
+ defer rows.Close()
+
+ // Загрузка данных в структуру Order
+ newOrder := &Order{}
+ newOrder.OrderItems = make([]OrderItem, 0)
+
+ for rows.Next() {
+  orderItem := OrderItem{}
+  item := Item{}
+  err = rows.Scan(&newOrder.ID, &newOrder.Status, &orderItem.ID, &orderItem.OrderID,
+   &orderItem.ItemID, &orderItem.Quantity, &item.ItemName, &item.Amount)
+  if err != nil {
+   log.Fatal(err)
+  }
+  orderItem.Item = item
+  newOrder.OrderItems = append(newOrder.OrderItems, orderItem)
+ }
+
+ fmt.Printf("%#v\n", newOrder)
 }
